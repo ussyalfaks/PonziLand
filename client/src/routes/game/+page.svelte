de9b1da06a8606a4f146208689ad4c49aea0ac5d
0xmugen <pierre.semanne@gmail.com>
2024-11-30 14:40:51 +0100

    feat: added sidebar

--- client/src/routes/game/+page.svelte
+++ client/src/routes/game/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import Map from '$lib/map/map.svelte';
+    import Ui from '$lib/ui/ui.svelte';
 </script>
 
 <div class="h-screen w-screen bg-black/10 overflow-hidden">
     <Map />
+    <Ui />
 </div>
