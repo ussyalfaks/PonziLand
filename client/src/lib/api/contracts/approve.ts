6747c412f9f50531d4f1e2755554a3c6013eed6d
Valentin Ricard <me@valentin.red>
2025-01-12 17:15:49 +0000

    fix: Removed web3 import, that is not used anymore

--- client/src/lib/api/contracts/approve.ts
+++ client/src/lib/api/contracts/approve.ts
@@ -13,9 +13,6 @@ import {
   type Call,
 } from 'starknet';
 
-import { Web3 } from 'web3';
-const web3 = new Web3();
-
 export type ApprovalData = {
   tokenAddress: string;
   amount: BigNumberish;
