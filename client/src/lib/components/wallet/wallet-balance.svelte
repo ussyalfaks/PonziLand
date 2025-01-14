eb626b5138c7c178da4135f592c2fedc8a8a1c1b
hadouin <had@runelabs.xyz>
2025-01-14 16:03:58 +0100

    fetch balances of kown tokens array

--- client/src/lib/components/wallet/wallet-balance.svelte
+++ client/src/lib/components/wallet/wallet-balance.svelte
@@ -3,21 +3,30 @@
   import { useDojo } from '$lib/contexts/dojo';
   import { dojoConfig } from '$lib/dojoConfig';
   import { DojoProvider } from '@dojoengine/core';
+  import data from '$lib/data.json';
 
   const { store, client: sdk, account } = useDojo();
 
   const accountData = $derived(account.getAccount());
 
-  let redBalance = $state();
-  let greenBalance = $state();
-  let blueBalance = $state();
+  let tokenBalances = $state<
+    { token: string; balance: Promise<bigint | null> }[]
+  >([]);
 
-  const toReadableFormat = (balance: unknown) => {
+  const toReadableFormat = (
+    balance: unknown,
+    precision: number = 4,
+    tokenDecimals: number = 18,
+  ) => {
     if (typeof balance !== 'bigint' && typeof balance !== 'number') {
       return balance;
     }
 
-    return BigInt(balance) / BigInt(10) ** BigInt(18);
+    const factor = BigInt(10) ** BigInt(tokenDecimals - precision);
+    const adjustedBalance = BigInt(balance) / factor;
+    const formattedBalance = Number(adjustedBalance) / 10 ** precision;
+
+    return formattedBalance.toPrecision(precision);
   };
 
   $effect(() => {
@@ -26,43 +35,26 @@
     }
 
     const provider = new DojoProvider(dojoConfig.manifest, dojoConfig.rpcUrl);
-    fetchTokenBalance(
-      '0x06450580d0d5cd36f0107227091ca68a237fd0ab538ae59ea43868f660bc2c30',
-      accountData,
-      provider,
-    ).then((res) => {
-      console.log('from component:', res);
-      redBalance = res;
-    });
-    fetchTokenBalance(
-      '0x04ed678da6c0534e8ba7a1e7db81f3ecc0f1c2628094094b5123c481cd13461f',
-      accountData,
-      provider,
-    ).then((res) => {
-      console.log('from component:', res);
-      greenBalance = res;
-    });
-    fetchTokenBalance(
-      '0x01853f03f808ae62dfbd8b8a4de08e2052388c40b9f91d626090de04bbc1f619',
-      accountData,
-      provider,
-    ).then((res) => {
-      console.log('from component:', res);
-      blueBalance = res;
+    tokenBalances = data.availableTokens.map((token) => {
+      const balance = fetchTokenBalance(token.address, accountData, provider);
+
+      return {
+        token: token.name,
+        balance,
+      };
     });
   });
 </script>
 
 <div>Wallet Balance</div>
-<div>
-  RED: {toReadableFormat(redBalance)}
-  <span class="text-gray-500">({redBalance})</span>
-</div>
-<div>
-  GREEN: {toReadableFormat(greenBalance)}
-  <span class="text-gray-500">({greenBalance})</span>
-</div>
-<div>
-  BLUE: {toReadableFormat(blueBalance)}
-  <span class="text-gray-500">({blueBalance})</span>
-</div>
+{#each tokenBalances as tokenBalance}
+  <div>
+    <span>{tokenBalance.token}:</span>
+    {#await tokenBalance.balance}
+      <div>Loading...</div>
+    {:then balance}
+      {toReadableFormat(balance)}
+      <span class="text-gray-500">({balance})</span>
+    {/await}
+  </div>
+{/each}
