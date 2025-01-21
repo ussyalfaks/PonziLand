47db0dee65fb76426d290cf070875df04a6efb22
0xmugen <pierre.semanne@gmail.com>
2025-01-21 20:03:13 +0100

    fix: frontend getTieldInfo call

--- client/src/lib/components/map/rates-overlay.svelte
+++ client/src/lib/components/map/rates-overlay.svelte
@@ -1,29 +1,26 @@
 <script lang="ts">
-  import { selectedLandMeta } from '$lib/stores/stores.svelte';
   import type { YieldInfo } from '$lib/interfaces';
+  import { type SelectedLandType } from '$lib/stores/stores.svelte';
+
+  let { land } = $props<{ land: SelectedLandType }>();
+
+  $inspect('land from rates-overlay', land);
 
   let yieldInfo: YieldInfo[] | undefined;
 
-  async function updateYieldInfo() {
-    yieldInfo = await $selectedLandMeta?.getYieldInfo();
-  }
-
   $effect(() => {
-    if ($selectedLandMeta) {
-      updateYieldInfo();
-    }
-  });
-
-  $effect(() => {
-    console.log(yieldInfo);
+    land.getYieldInfo().then((res: YieldInfo[]) => {
+      yieldInfo = res;
+      $inspect('yeildInfo', yieldInfo);
+    });
   });
 </script>
 
-<div
+<!-- <div
   class="absolute inset-0 grid grid-cols-3 grid-rows-3"
   style="transform: translate(-33.33%, -33.33%); width: 300%; height: 300%;"
 >
   {#each Array(9) as _, i}
     <div class="border-2 border-blue-400/50 bg-blue-400/10"></div>
   {/each}
-</div>
+</div> -->
