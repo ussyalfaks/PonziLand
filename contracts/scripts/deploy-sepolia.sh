35c9a44cf913c4a0b1420c7ec6a33901aeea2fee
Valentin Ricard <me@valentin.red>
2025-01-23 11:58:18 +0100

    chore: Deployed new world

--- contracts/scripts/deploy-sepolia.sh
+++ contracts/scripts/deploy-sepolia.sh
@@ -2,10 +2,10 @@
 
 # Stop script on error
 set -e
- 
+
 # Load environment variables from the appropriate file
 ENV_FILE=".env.sepolia"
- 
+
 if [ -f "$ENV_FILE" ]; then
   echo "Loading environment variables from $ENV_FILE..."
   export $(grep -v '^#' "$ENV_FILE" | xargs)
@@ -13,7 +13,7 @@ else
   echo "Environment file $ENV_FILE not found!"
   exit 1
 fi
- 
+
 # Define a cleanup function to clear environment variables
 cleanup_env() {
   echo "Cleaning up environment variables..."
@@ -22,18 +22,18 @@ cleanup_env() {
   unset DOJO_PRIVATE_KEY
   echo "Environment variables cleared."
 }
- 
+
 # Set the trap to execute cleanup on script exit or error
 trap cleanup_env EXIT
- 
+
 # Build the project
 echo "Building the project..."
 sozo -P sepolia build
- 
+
 # Deploy the project
 echo "Deploying to Sepolia..."
-sozo -P sepolia migrate
- 
+sozo -P sepolia migrate --fee ETH
+
 # Deployment succeeded message
 echo "Deployment completed successfully."
 
