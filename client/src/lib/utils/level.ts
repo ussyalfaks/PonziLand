5a4e3ef1ac95d9d5fbab87f4bb652bf234132266
Valentin Ricard <me@valentin.red>
2025-02-13 09:41:21 +0100

    fix: Fixed leveliing when reaching past level 1

--- client/src/lib/utils/level.ts
+++ client/src/lib/utils/level.ts
@@ -2,6 +2,12 @@ import type { Level } from '$lib/api/land.svelte';
 import type { LevelEnum as LevelModel } from '$lib/models.gen';
 
 export function fromDojoLevel(level: LevelModel): Level | undefined {
+  if (typeof level == 'string') {
+    return level as Level;
+  }
+
+  console.log('level:', level);
+
   return (
     (Object.entries(level ?? {}).filter(
       ([k, v]) => v != undefined,
