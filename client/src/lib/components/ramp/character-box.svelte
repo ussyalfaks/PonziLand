1d2f36c564f0f8aac08f92804714b3db187ca8a1
Veikkan <pierre.semanne@gmail.com>
2025-02-24 02:11:44 +0100

    feat: chat box + trancuated network info

---
+++ client/src/lib/components/ramp/character-box.svelte
@@ -0,0 +1,18 @@
+<script lang="ts"></script>
+
+<div
+  class="absolute flex items-center top-0 left-1/2 transform z-30 -translate-x-1/2 text-white p-4 my-2"
+>
+  <img
+    src="/assets/ui/ramp/character.png"
+    alt="Character"
+    width="100"
+    height="100"
+  />
+  <div
+    class="bg-chatbox w-600 h-100 flex items-center justify-center"
+    style="background-image: url('/assets/ui/ramp/chat-box.png'); background-size: cover; width: 600px; height: 180px;"
+  >
+    <p>Your filler text goes here</p>
+  </div>
+</div>
