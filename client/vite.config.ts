a5ee02c04f326eaab5db4572ae071bcc4a427efc
hadouin <had@runelabs.xyz>
2025-03-10 18:32:57 +0530

    add mkcert for controller on local host

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import wasm from 'vite-plugin-wasm';
 import { defineConfig } from 'vitest/config';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [sveltekit(), wasm(), topLevelAwait()],
+  plugins: [sveltekit(), wasm(), topLevelAwait(), mkcert()],
   build: {
     sourcemap: false,
     minify: false,
