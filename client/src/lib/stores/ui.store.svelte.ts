9402e75d27d03482047ac48a2f78de9a55f11bd8
hadouin <had@runelabs.xyz>
2025-03-11 12:15:53 +0530

    prettier

--- client/src/lib/stores/ui.store.svelte.ts
+++ client/src/lib/stores/ui.store.svelte.ts
@@ -1,13 +1,13 @@
-import type { TileInfo } from "$lib/interfaces";
+import type { TileInfo } from '$lib/interfaces';
 
 export let uiStore = $state<{
   showModal: boolean;
   modalType: 'bid' | 'buy' | 'land-info' | null;
   modalData: TileInfo | null;
-  toolbarActive : 'lands' | 'notifications' | 'auctions' | null
+  toolbarActive: 'lands' | 'notifications' | 'auctions' | null;
 }>({
   showModal: false,
   modalType: null,
   modalData: null,
-  toolbarActive: null
+  toolbarActive: null,
 });
