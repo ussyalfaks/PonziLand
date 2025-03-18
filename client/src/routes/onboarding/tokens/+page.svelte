92c353fc18fe215af9549dd36a5ad2760242f4ac
Valentin Ricard <me@valentin.red>
2025-03-18 17:35:30 +0100

    fix: Fixed open on start

--- client/src/routes/onboarding/tokens/+page.svelte
+++ client/src/routes/onboarding/tokens/+page.svelte
@@ -12,7 +12,7 @@
 <div class="flex flex-col h-full grow p-5 gap-2">
   <h1 class="text-2xl font-bold self-center mb-5">Claim your tokens!</h1>
 
-  <Button onclick={getSocialink().claimTokens()}
+  <Button onclick={() => getSocialink().claimTokens()}
     >Check your claiming status</Button
   >
 </div>
