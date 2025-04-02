adca32981b7bf9d695793a5cf8838c6e62b67aae
Veikkan <pierre.semanne@gmail.com>
2025-04-02 15:43:32 +0200

    feat: added basic tutorial steps

---
+++ client/src/lib/components/tutorial/stores.ts
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store';
+
+export const tutorialProgression = writable<number>(0);
---
+++ client/src/lib/components/tutorial/tuto-progression.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { tutorialProgression } from './stores';
+</script>
+
+<div
+  class="fixed bottom-8 right-24 z-[9999] pointer-events-none flex flex-col items-center"
+>
+  <img
+    src="/tutorial/Ponzi_Arrow.png"
+    alt="Ponzi Arrow"
+    class="w-[60px] h-auto scale-x-[-1]"
+  />
+  <span class="text-white text-ponzi font-bold mt-1 text-sm">Next</span>
+</div>
+
+<div
+  class="fixed bottom-16 left-0 right-0 mx-auto w-fit z-[9999] pointer-events-none flex flex-col items-center"
+>
+  <span class="text-white text-ponzi font-bold text-2xl">
+    Step {$tutorialProgression}/15
+  </span>
+</div>
+
+<div
+  class="fixed bottom-8 left-24 z-[9999] pointer-events-none flex flex-col items-center"
+>
+  <img
+    src="/tutorial/Ponzi_Arrow.png"
+    alt="Ponzi Arrow"
+    class="w-[60px] h-auto"
+  />
+  <span class="text-white text-ponzi font-bold mt-1 text-sm">Previous</span>
+</div>
