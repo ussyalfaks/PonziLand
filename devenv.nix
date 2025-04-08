613a72f47f80b5b88757a0da75e45e21bf40926e
Valentin Ricard <me@valentin.red>
2025-04-08 15:20:47 +0200

    chore: Updated nix deployment

--- devenv.nix
+++ devenv.nix
@@ -7,13 +7,12 @@
   ...
 }: let
   system = pkgs.stdenv.system;
-  dojoVersion = "1.0.12";
   dojo-nix = inputs.cairo-nix.legacyPackages.${system};
   cairo-nix = inputs.cairo-nix.packages.${system};
 in {
   packages = with pkgs; [
     git
-    dojo-nix.dojo.${dojoVersion}.all
+    cairo-nix.dojo
     cairo-nix.scarb
     cairo-nix.starkli
     cairo-nix.slot
