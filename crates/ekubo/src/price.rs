e584ba58efd327a338e683323ff780cf1257e2a5
Valentin Ricard <me@valentin.red>
2025-04-13 20:40:46 +0200

    feat: Added ekubo price calculations

---
+++ crates/ekubo/src/price.rs
@@ -0,0 +1,25 @@
+use std::fmt::Display;
+
+use crate::math::u256fd128::U256FD128;
+
+pub struct PairRatio(pub U256FD128);
+
+impl PairRatio {
+    pub fn inverse(&self) -> Self {
+        PairRatio(U256FD128::from_whole(1) / self.0)
+    }
+}
+
+impl std::ops::Deref for PairRatio {
+    type Target = U256FD128;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Display for PairRatio {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
