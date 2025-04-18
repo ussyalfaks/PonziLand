b98155ced93abfc6734743aaf3152b00e7bd9893
Gianfranco99 <67334390+Gianfranco99@users.noreply.github.com>
2025-04-18 16:27:38 +0000

    change values for auction modal (#349)
    # Update default values for auction modal
    
    this pr resolves #334
    Changed the default values in the auction modal:
    - Increased stake amount from 0.0000001 to 10
    - Increased sell amount from 0.0001 to 1

--- client/src/lib/components/auction/auction-modal.svelte
+++ client/src/lib/components/auction/auction-modal.svelte
@@ -40,10 +40,8 @@
   // Form
   let selectedToken = $state<Token | undefined>();
   //TODO: Change defaults values into an error component
-  let stakeAmount = $state<CurrencyAmount>(
-    CurrencyAmount.fromScaled('0.0000001'),
-  );
-  let sellAmount = $state<CurrencyAmount>(CurrencyAmount.fromScaled('0.0001'));
+  let stakeAmount = $state<CurrencyAmount>(CurrencyAmount.fromScaled('10'));
+  let sellAmount = $state<CurrencyAmount>(CurrencyAmount.fromScaled('1'));
 
   async function handleBiddingClick() {
     loading = true;
