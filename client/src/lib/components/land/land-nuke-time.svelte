f58440bac025dc7efdc8602ce7019bfc844bf82a
0xmugen <pierre.semanne@gmail.com>
2025-04-21 19:11:11 +0200

    fix: nuke display

--- client/src/lib/components/land/land-nuke-time.svelte
+++ client/src/lib/components/land/land-nuke-time.svelte
@@ -22,13 +22,17 @@
       ),
     ).toString();
   }
+
+  function formatNukeTime(nukeTime: string | undefined) {
+    return nukeTime === '' ? 'NUKABLE!' : nukeTime;
+  }
 </script>
 
 <div class="absolute right-0 -translate-y-12">
   <Card>
     <div class="flex items-center gap-2 text-ponzi-number text-red-500">
       <img src="/extra/nuke.png" alt="Nuke Shield" class="h-6 w-6" />
-      <span>{nukeTime}</span>
+      <span>{formatNukeTime(nukeTime)}</span>
     </div>
   </Card>
 </div>
