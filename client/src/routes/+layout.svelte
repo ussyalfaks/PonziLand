1a7b8a1a14edff751d0a866a856b30b25f940f7c
0xMugen <45738409+0xMugen@users.noreply.github.com>
2025-04-25 13:24:08 +0000

    feat: some basic seo (#464)

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -7,5 +7,31 @@
   setupAccount();
 </script>
 
+<svelte:head>
+  <title>Ponziland - The Ultimate DeFi Game</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta
+    name="description"
+    content="Ponziland is a fully token-agnostic DeFi game where you conquer lands, stake tokens, nuke rivals, and build empires."
+  />
+  <meta property="og:title" content="Ponziland - The Ultimate DeFi Game" />
+  <meta
+    property="og:description"
+    content="Conquer, stake, and nuke your way through a wild DeFi metagame."
+  />
+  <meta property="og:image" content="https://play.ponzi.land/logo.png" />
+  <meta property="og:url" content="https://ponzi.land" />
+
+  //Twitter
+  <meta name="twitter:card" content="https://play.ponzi.land/home/hero.png" />
+  <meta name="twitter:title" content="Ponziland - The Ultimate DeFi Game" />
+  <meta
+    name="twitter:description"
+    content="Conquer, stake, and nuke your way through a wild DeFi metagame."
+  />
+  <meta name="twitter:image" content="https://play.ponzi.land/home/hero.png" />
+</svelte:head>
+
 <SelectWalletModal />
 <slot />
