69eb9d94a3d6dbc27bffcc2eaf8af4d614362e28
hadouin <79368202+hadouin@users.noreply.github.com>
2025-06-05 12:56:16 +0000

    feat: moooooore satisfaction on claim (#648)

--- client/src/lib/components/+game-ui/widgets/wallet/token-value-display.svelte
+++ client/src/lib/components/+game-ui/widgets/wallet/token-value-display.svelte
@@ -6,6 +6,7 @@
   import { displayCurrency } from '$lib/utils/currency';
   import { CurrencyAmount } from '$lib/utils/CurrencyAmount';
   import { Tween } from 'svelte/motion';
+  import data from '$profileData';
 
   let { amount, token }: { amount: bigint; token: Token } = $props<{
     amount: bigint;
@@ -87,39 +88,28 @@
 
 <div class="flex flex-1 items-center justify-between text-xl tracking-wide">
   <div
-    class="font-ds opacity-75 text-[#6BD5DD]{animating
-      ? 'animating text-green-500'
+    class="gap-1 flex font-ds opacity-75 text-[#6BD5DD]{animating
+      ? 'animating scale-110 text-yellow-500 font-bold'
       : ''}"
   >
-    {CurrencyAmount.fromUnscaled(tweenAmount.current, token)}
+    <div>{CurrencyAmount.fromUnscaled(tweenAmount.current, token)}</div>
+    <div class="relative">
+      {#if animating}
+        <span class="absolute left-0 animate-in-out-left">
+          +{CurrencyAmount.fromUnscaled(increment, token)}
+        </span>
+      {/if}
+    </div>
   </div>
   <div class="font-ds opacity-75 text-[#D9D9D9]">
     {token.symbol}
   </div>
 </div>
 
-<!-- <div class="flex gap-2 justify-end w-full text-stroke-none">
-  <div class="flex flex-col items-end">
-    <div class="flex gap-2 items-center justify-end w-full">
-      <div class="flex text-right justify-end w-full relative overflow-hidden">
-        {#if animating}
-          <span class="ml-3 absolute left-0 top-0 animate-in-out-left">
-            +{CurrencyAmount.fromUnscaled(increment, token)}
-          </span>
-        {/if}
-      </div>
-    </div>
-    <span class=" text-gray-500">
-      {baseTokenValue.toString()}
-      {data.mainCurrency}
-    </span>
-  </div>
-</div> -->
-
 <style>
   @keyframes scale-down {
     from {
-      transform: scale(1.05);
+      transform: scale(1.2);
     }
     to {
       transform: scale(1);
@@ -127,8 +117,8 @@
   }
 
   .animating {
-    transform: scale(1.05);
-    animation: scale-down 0.3s ease-in-out 1000ms forwards;
+    transform: scale(1.2);
+    animation: scale-down 1s ease-in-out 1000ms forwards;
   }
 
   @keyframes slideInOutLeft {
